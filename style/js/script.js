453ec09723379326e0bb37cab61fbf8d1c0905b7
Shashank <shashankgowdar02@gmail.com>
2023-04-14 14:02:50 +0530

    page made responsive

--- style/js/script.js
+++ style/js/script.js
@@ -1,7 +1,13 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// let menu = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+// menu.onclick = () => {
+//     menu.classList.toggle('bx-x');
+//     navbar.classList.toggle('open');
+// }
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
 }
